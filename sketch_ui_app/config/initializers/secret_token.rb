bf7393e5a06e9adeb8d0b8768b13d6e9ccb5e683
Atul Verma <atulverma82@yahoo.com>
2013-11-11 23:21:28 +0530

    new rails app

---
+++ sketch_ui_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SketchUiApp::Application.config.secret_key_base = 'a6e34f0b7a201a80c58c1e0216e54c8f60c251d36eb449baf602de6f902fa1194dae1e4ec33dcbbb2cb7aaf36b16a4e7663f63888be92add788bdcb302d3d2dc'
