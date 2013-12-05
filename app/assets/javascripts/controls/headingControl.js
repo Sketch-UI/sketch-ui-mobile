c9cfbc7b907b9906a03084534026b37d4df25cd5
Inderpal Singh <inderps@thoughtworks.com>
2013-12-05 20:08:19 +0530

    added headings control via rake task

---
+++ app/assets/javascripts/controls/headingControl.js
@@ -0,0 +1,8 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['HeadingControl'] = {
+  initialData: {
+            content: "Hello! This is a heading",
+            size: "36"
+  },
+  propertyWindowCallback: function(ractiveControl){}
+};
