1dacf6eab9e77ab9bdedb668ba1bc7e8653417fb
Inderpal Singh <inderps@thoughtworks.com>
2013-12-05 18:43:06 +0530

    added pagination control

--- app/assets/javascripts/controlsMetadata.js
+++ app/assets/javascripts/controlsMetadata.js
@@ -5,7 +5,7 @@ var ControlsMetadata = {
         initialData: {
             button_label: "Submit"
         },
-        callback: function(control){
+        callback: function (control) {
         }
     },
 
@@ -16,7 +16,7 @@ var ControlsMetadata = {
             textvalue: "Harsha",
             width: "50"
         },
-        callback: function(control){
+        callback: function (control) {
         }
     },
 
@@ -27,7 +27,8 @@ var ControlsMetadata = {
             content: "Hello! This is a heading",
             size: "36"
         },
-        callback: function(control){
+        callback: function (control) {
+
         }
     },
 
@@ -35,26 +36,55 @@ var ControlsMetadata = {
         template: "button-group-control-template",
         propertyWindowTemplate: "button-group-property-template",
         initialData: {
-            noOfButtons:3,
-            buttonLabels: ["button1", "button2","button3"]
+            noOfButtons: 3,
+            buttonLabels: ["button1", "button2", "button3"]
 
 
         },
-        callback: function(control){
+        callback: function (control) {
             $("#properties-container .noOfButtons").val($("#properties-container .noOfButtons").data("value"));
 
-            $("#properties-container .noOfButtons").change(function(){
+            $("#properties-container .noOfButtons").change(function () {
                 var noOfButtons = $("#properties-container .noOfButtons").val();
                 var buttonLabels = control.get("buttonLabels");
                 control.set("noOfButtons", noOfButtons);
 
-                    while(noOfButtons>buttonLabels.length){
-                        buttonLabels.push("button" + (buttonLabels.length + 1) );
-                    }
-                    while(noOfButtons<buttonLabels.length){
-                       buttonLabels.pop();
-                    }
+                while (noOfButtons > buttonLabels.length) {
+                    buttonLabels.push("button" + (buttonLabels.length + 1));
+                }
+                while (noOfButtons < buttonLabels.length) {
+                    buttonLabels.pop();
+                }
             });
+        }
+    },
+
+    "PaginationControl": {
+        template: "pagination-control-template",
+        propertyWindowTemplate: "pagination-property-template",
+        initialData: {
+            starting_no: 1,
+            no_of_pages: 3
+        },
+        callback: function (control) {
+            control.set("pages", getPages());
+            $(".properties .no_of_pages").change(function () {
+                control.set("pages", getPages());
+            });
+            $(".properties .starting_no").change(function () {
+                control.set("pages", getPages());
+            });
+
+            function getPages() {
+                var pages = [],
+                    startingNo = +$(".properties .starting_no").val(),
+                    noOfPages = +$(".properties .no_of_pages").val(),
+                    range = startingNo + noOfPages;
+                for (var i = startingNo; i < range; i++) {
+                    pages.push(i);
+                }
+                return pages;
+            }
         }
     }
 };
