ee8a742466f75663461dfeee068f1324e2d22151
Inderpal Singh <inderps@thoughtworks.com>
2013-12-05 20:05:47 +0530

    added textfield via rake task

---
+++ app/assets/javascripts/controls/textboxControl.js
@@ -0,0 +1,8 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['TextboxControl'] = {
+  initialData: {
+      textvalue: "Harsha",
+      width: "50"
+  },
+  propertyWindowCallback: function(ractiveControl){}
+};
