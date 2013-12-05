2d0b2f7697755bef4a75af680e074391690f82c3
Inderpal Singh <inderps@thoughtworks.com>
2013-12-05 20:02:57 +0530

    added button control via rake task

---
+++ app/assets/javascripts/controls/buttonControl.js
@@ -0,0 +1,7 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['ButtonControl'] = {
+  initialData: {
+      button_label: "Submit"
+  },
+  propertyWindowCallback: function(ractiveControl){}
+};
