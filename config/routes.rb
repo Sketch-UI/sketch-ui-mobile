9ad2a7e458dd89c94d846cf843cf47b326b06a01
Inderpal Singh <inderps@thoughtworks.com>
2013-12-05 18:46:31 +0530

    changed root route to designer

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ SketchUi::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
-
-  get 'designer/' => 'designer#index', as: :designer
+  root 'designer#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/routing/route_spec.rb
+++ spec/routing/route_spec.rb
@@ -4,14 +4,8 @@ require 'spec_helper'
 describe "routes" do
 
   context "root" do
-    it "should route to root" do
-      assert_routing({ path: "/", method: :get }, { controller: "home", action: "index" })
-    end
-  end
-
-  context "designer" do
-    it "should route to index action" do
-      assert_routing({ path: "/designer", method: :get }, { controller: "designer", action: "index" })
+    it "should route to designer screen" do
+      assert_routing({ path: "/", method: :get }, { controller: "designer", action: "index" })
     end
   end
 end
