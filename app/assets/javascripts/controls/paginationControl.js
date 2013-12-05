219d358f2c05c5b3944b757b5f788b2a265312d0
Inderpal Singh <inderps@thoughtworks.com>
2013-12-05 20:23:00 +0530

    fixed pagination control to take initial data on select

--- app/assets/javascripts/controls/paginationControl.js
+++ app/assets/javascripts/controls/paginationControl.js
@@ -2,26 +2,27 @@ window.ControlsMetadata = window.ControlsMetadata || {};
 window.ControlsMetadata['PaginationControl'] = {
   initialData: {
       starting_no: 1,
-      no_of_pages: 3
+      no_of_pages: 3,
+      pages: [1,2,3]
   },
   propertyWindowCallback: function(ractiveControl){
-      ractiveControl.set("pages", getPages());
+      var _this = this;
       $(".properties .no_of_pages").change(function () {
-          ractiveControl.set("pages", getPages());
+          ractiveControl.set("pages", _this.getPages());
       });
       $(".properties .starting_no").change(function () {
-          ractiveControl.set("pages", getPages());
+          ractiveControl.set("pages", _this.getPages());
       });
+  },
 
-      function getPages() {
-          var pages = [],
-              startingNo = +$(".properties .starting_no").val(),
-              noOfPages = +$(".properties .no_of_pages").val(),
-              range = startingNo + noOfPages;
-          for (var i = startingNo; i < range; i++) {
-              pages.push(i);
-          }
-          return pages;
+  getPages: function() {
+      var pages = [],
+          startingNo = +$(".properties .starting_no").val(),
+          noOfPages = +$(".properties .no_of_pages").val(),
+          range = startingNo + noOfPages;
+      for (var i = startingNo; i < range; i++) {
+          pages.push(i);
       }
+      return pages;
   }
 };
