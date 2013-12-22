6ae73930152c3bc28a2cce73f4ebb1893a1142d0
kushjain@thoughtworks.com <Mr.KushJain@gmail.com>
2013-12-22 14:24:26 +0530

    Modified case of jquery in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
-//= require jquery/jquery-2.0.3
-//= require jquery/jquery-migrate-1.2.1
-//= require jquery/jquery-ui-1.10.3.custom.min
-//= require jquery/jquery.mCustomScrollbar.min
-//= require jquery/jquery.serializeJSON
+//= require jQuery/jquery-2.0.3
+//= require jQuery/jquery-migrate-1.2.1
+//= require jQuery/jquery-ui-1.10.3.custom.min
+//= require jQuery/jquery.mCustomScrollbar.min
+//= require jQuery/jquery.serializeJSON
 //= require bootstrap/bootstrap.min
 //= require Ractive/Ractive
 //= require draggabilly/draggabilly
