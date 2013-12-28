068c5db7d64dd20c4c0ae04903ef0c869e123a24
kushjain@thoughtworks.com <Mr.KushJain@gmail.com>
2013-12-28 18:35:10 +0530

    Added Link Control

---
+++ app/assets/javascripts/controls/linkControl.js
@@ -0,0 +1,8 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['LinkControl'] = {
+  initialData: {
+      display_text:"link",
+      url:"http://sketch-ui.herokuapp.com"
+  },
+  propertyWindowCallback: function(ractiveControl){}
+};
