4929fdbb88f0f5c75f33b3cafeca86939417a891
kushjain@thoughtworks.com <Mr.KushJain@gmail.com>
2013-12-28 14:43:34 +0530

    Added Text Control to enter caption

---
+++ app/assets/javascripts/controls/textControl.js
@@ -0,0 +1,7 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['TextControl'] = {
+  initialData: {
+      caption:'Enter Some non-editable text here'
+  },
+  propertyWindowCallback: function(ractiveControl){}
+};
