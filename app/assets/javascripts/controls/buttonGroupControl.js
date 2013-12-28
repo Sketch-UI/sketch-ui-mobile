9a20876bd1d54537c279641d045c8d19c14389d5
kushjain@thoughtworks.com <Mr.KushJain@gmail.com>
2013-12-28 16:54:39 +0530

    Changed Button Group Property Window to take comma seprated captions

--- app/assets/javascripts/controls/buttonGroupControl.js
+++ app/assets/javascripts/controls/buttonGroupControl.js
@@ -1,23 +1,20 @@
 window.ControlsMetadata = window.ControlsMetadata || {};
 window.ControlsMetadata['ButtonGroupControl'] = {
   initialData: {
-      noOfButtons: 3,
+      buttons:"button1,button2,button3",
       buttonLabels: ["button1", "button2", "button3"]
   },
   propertyWindowCallback: function(ractiveControl){
-      $("#properties-container .noOfButtons").val($("#properties-container .noOfButtons").data("value"));
 
-      $("#properties-container .noOfButtons").change(function () {
-          var noOfButtons = $("#properties-container .noOfButtons").val();
-          var buttonLabels = ractiveControl.get("buttonLabels");
-          ractiveControl.set("noOfButtons", noOfButtons);
+      $("#properties-container .buttons").change(function () {
+          var buttons = $("#properties-container .buttons").val();
+          var buttonLabels = buttons.split(',');
+          for(var i = 0; i < buttonLabels.length; i++)
+          {
+              buttonLabels[i] = buttonLabels[i].replace(/^\s*/, "").replace(/\s*$/, "");
+          }
+          ractiveControl.set("buttonLabels", buttonLabels)
 
-          while (noOfButtons > buttonLabels.length) {
-              buttonLabels.push("button" + (buttonLabels.length + 1));
-          }
-          while (noOfButtons < buttonLabels.length) {
-              buttonLabels.pop();
-          }
       });
   }
 };
