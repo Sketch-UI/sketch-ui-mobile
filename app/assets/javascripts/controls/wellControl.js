f5596aaf2cc369d8b09aef5a77399590477e1392
kushjain@thoughtworks.com <Mr.KushJain@gmail.com>
2014-01-06 19:39:00 +0530

    +Vaibhav|Added well control

---
+++ app/assets/javascripts/controls/wellControl.js
@@ -0,0 +1,7 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['WellControl'] = {
+  initialData: {
+      content:'Your Content'
+  },
+  propertyWindowCallback: function(ractiveControl){}
+};
