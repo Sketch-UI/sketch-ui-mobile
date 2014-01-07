a297fa85b755586dab81a8329621d52c16810b44
Shreya Pandit <shreyapandit@thoughtworks.com>
2014-01-07 20:31:51 +0530

    Adding various types of progress bars

--- app/assets/javascripts/controls/progressBarControl.js
+++ app/assets/javascripts/controls/progressBarControl.js
@@ -1,8 +1,12 @@
 window.ControlsMetadata = window.ControlsMetadata || {};
 window.ControlsMetadata['ProgressBarControl'] = {
   initialData: {
-    width: '50'
+    width: '50',
+    progress_bar_type:'info'
   },
   propertyWindowCallback: function(ractiveControl){
+      $("#progressbar-type").change(function(ev){
+          ractiveControl.set("progress_bar_type", $("#progressbar-type").val());
+      });
   }
 };
