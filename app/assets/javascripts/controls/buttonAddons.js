67bcd048b7cac2f734ed6ff350ad47af85011be8
Ruchi Mutneja <Ruchi@inruchim.corporate.thoughtworks.com>
2014-01-07 20:31:33 +0530

    Adding button addon control

---
+++ app/assets/javascripts/controls/buttonAddons.js
@@ -0,0 +1,8 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['ButtonAddons'] = {
+  initialData: {
+    Button_addons_label: "Go!",
+    width: "300"
+  },
+  propertyWindowCallback: function(ractiveControl){}
+};
