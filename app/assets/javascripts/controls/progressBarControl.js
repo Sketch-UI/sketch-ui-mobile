8a66fbac19318b228ea4d5f402999533b81379f3
Shreya Pandit <shreyapandit@thoughtworks.com>
2014-01-07 19:53:51 +0530

    +Kush|Added progress bar control

---
+++ app/assets/javascripts/controls/progressBarControl.js
@@ -0,0 +1,8 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['ProgressBarControl'] = {
+  initialData: {
+  	width: '100px'
+  },
+  propertyWindowCallback: function(ractiveControl){
+  }
+};
