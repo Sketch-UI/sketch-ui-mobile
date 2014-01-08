39d703a6ce1e0d19efd587b5a2a664adb182497d
Vishal Sharma <vishalsh@thoughtworks.com>
2014-01-08 19:00:42 +0530

    + Pallavi Added color picker to set the background and text color of the button

--- app/assets/javascripts/controls/buttonControl.js
+++ app/assets/javascripts/controls/buttonControl.js
@@ -1,11 +1,33 @@
 window.ControlsMetadata = window.ControlsMetadata || {};
 window.ControlsMetadata['ButtonControl'] = {
-  initialData: {
-      button_label: "Submit"
-  },
-  propertyWindowCallback: function(ractiveControl){
-        $(".control button").dblclick(function(){
+    initialData: {
+        button_label: "Submit",
+        background_color: "lightgray",
+        text_color: "black"
+    },
+    propertyWindowCallback: function (ractiveControl) {
+        $(".control button").dblclick(function () {
             $("#buttonLabel").focus();
         });
-  }
+
+        var colorPicker = function (element, styleProperty) {
+            $(element).colpick({
+                layout: 'hex',
+                submit: 0,
+                colorScheme: 'dark',
+                onChange: function (hsb, hex, rgb, fromSetColor) {
+                    if (!fromSetColor) {
+                        $(element).val('#' + hex).css('border-color', '#' + hex)
+                        ractiveControl.set(styleProperty, '#' + hex);
+                    }
+                }
+            })
+                .keyup(function () {
+                    $(this).colpickSetColor(this.value);
+                })
+        }
+
+        $('#picker').focus(colorPicker($('#backgroundPicker'), "background_color"));
+        $('#textPicker').focus(colorPicker($('#textPicker'), "text_color"));
+    }
 };
