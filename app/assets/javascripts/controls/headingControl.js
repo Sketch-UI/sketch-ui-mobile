d1cff2858d9917473288f026de7d1446387e67de
Inderpal Singh <inderps@thoughtworks.com>
2014-01-12 22:56:54 +0530

    adding more properties for headings

--- app/assets/javascripts/controls/headingControl.js
+++ app/assets/javascripts/controls/headingControl.js
@@ -2,7 +2,8 @@ window.ControlsMetadata = window.ControlsMetadata || {};
 window.ControlsMetadata['HeadingControl'] = {
   initialData: {
             content: "Hello! This is a heading",
-            size: "36"
+            size: "36",
+            color: "#000000"
   },
   propertyWindowCallback: function(ractiveControl){}
 };
