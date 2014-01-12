e7a37051478969dd100e66a93e85225a7cb523df
Inderpal Singh <inderps@thoughtworks.com>
2014-01-12 16:13:09 +0530

    Pressing 'p' always goes in Preview mode bug fixed

--- app/assets/javascripts/previewBinding.js
+++ app/assets/javascripts/previewBinding.js
@@ -2,7 +2,7 @@ var PreviewBinding = (function() {
 
     var init = function() {
         $(document).keydown(function(ev) {
-            if(ev.keyCode == 80){
+            if(ev.keyCode == 80 && ev.target.nodeName.toLowerCase() !== 'input'){
                 $("#toolbox-left").toggle();
                 $("body").toggleClass("preview");
             }
