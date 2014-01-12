2398fba5123cf6749e0ef4c8af9d74c415b06d1f
Inderpal Singh <inderps@thoughtworks.com>
2014-01-12 20:38:02 +0530

    improved text control

--- app/assets/javascripts/controls/textControl.js
+++ app/assets/javascripts/controls/textControl.js
@@ -1,7 +1,14 @@
 window.ControlsMetadata = window.ControlsMetadata || {};
 window.ControlsMetadata['TextControl'] = {
   initialData: {
-      caption:'Enter Some non-editable text here'
+      caption:'Enter Some non-editable text here. Enter Some non-editable text here',
+      height: 75,
+      width: 180,
+      color: "#000000"
   },
-  propertyWindowCallback: function(ractiveControl){}
+  propertyWindowCallback: function(ractiveControl){
+      $("#free-text").change(function(ev){
+            ractiveControl.set("caption", $("#free-text").val());
+      });
+  }
 };
