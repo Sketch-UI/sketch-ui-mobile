266c801fcce8d2c07b85460af68231ee1b6ee2ea
Shreya Pandit <shreyapandit@thoughtworks.com>
2014-01-12 21:54:56 +0530

    +IPS| Changing approach of sitching content in tabbed display

--- app/assets/javascripts/controls/tabControl.js
+++ app/assets/javascripts/controls/tabControl.js
@@ -1,60 +1,31 @@
-
-  var combined_data= [];
-
 window.ControlsMetadata = window.ControlsMetadata || {};
 window.ControlsMetadata['TabControl'] = {
   initialData: {
-    tab_headers: ["Tab1","Tab2"],
-    tab_content: ["First Tab","Another Tab"],
-    combined_data: [{content: "First Tab",heading: "Tab1",href: "#Tab1"},{content: "Another Tab",heading: "Tab2",href: "#Tab2"}]
+    items: [
+        { item: "Item1", index: 0, active: true}, 
+        { item: "Item2", index: 1, active: false}
+    ],
+    width: 500,
+    height: 300
   },
   propertyWindowCallback: function(ractiveControl){
-    $('#myTab a').click(function (e) {
-      if($(this).parent('li').hasClass('active')){
-          $( $(this).attr('href') ).hide();
-      }
-      else {
-          e.preventDefault();
-          $(this).tab('show');
-      }
-  });
-
-    $("#properties-container .headers").change(function () {
-        combined_data =[]
-        var headers = $("#properties-container .headers").val();
-        var headerLabels = headers.split(',');
-        var i=0;
-        for(; i < headerLabels.length; i++)
-        {
-            headerLabels[i] = headerLabels[i].replace(/^\s*/, "").replace(/\s*$/, "");
-            // combined_data.push({
-            //   href:   "#"+headerLabels[i],
-            //   content: headerLabels[i]
-            // });
-        }
-        ractiveControl.set("headerLabels", headerLabels);
-    });
-
-    $("#properties-container .content").change(function () {
-        combined_data =[]
-        var headers = $("#properties-container .headers").val();
-        var headerLabels = headers.split(',');
-        var content = $("#properties-container .content").val();
-        var contentLabels = content.split(',');
-        var i=0;
-        for(; i < contentLabels.length; i++)
-        {
-            contentLabels[i] = contentLabels[i].replace(/^\s*/, "").replace(/\s*$/, "");
-            combined_data.push({
-              heading: headerLabels[i],
-              href:   "#"+headerLabels[i],
-              content: contentLabels[i]
-            });
-        }
-        ractiveControl.set("combined_data", combined_data);
-    });
-    $("li[id='First Tab']:first").addClass("active");
-    $("div[id='Tab1']:first").addClass("in active");
+      $("#properties-container .headers").change(function(ev){
+            var items = $("#properties-container .headers").val().split(",");
+            var formattedItems = [];
+            for(var i=0;i<items.length;i++){
+                var item = items[i];
+                var active=false;
+                if(item.substr(item.length - 3) == "***"){
+                    item = item.substring(0, item.length-3);
+                    active=true;
+                }
+                formattedItems.push({
+                    index: i,
+                    item: item,
+                    active: active
+                });
+            }
+            ractiveControl.set("items", formattedItems);
+      });
   }
-
 };
