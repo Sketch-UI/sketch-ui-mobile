fe15fd6765947a7c78021e336f0ac421a09acc9c
Paras Narang <narangparas@gmail.com>
2014-01-13 22:09:50 +0530

    fix #19 for Basic Tab Display control

--- app/assets/javascripts/controls/tabControl.js
+++ app/assets/javascripts/controls/tabControl.js
@@ -1,6 +1,7 @@
 window.ControlsMetadata = window.ControlsMetadata || {};
 window.ControlsMetadata['TabControl'] = {
   initialData: {
+    itemsString: "Item1***,Item2",
     items: [
         { item: "Item1", index: 0, active: true}, 
         { item: "Item2", index: 1, active: false}
@@ -9,8 +10,9 @@ window.ControlsMetadata['TabControl'] = {
     height: 300
   },
   propertyWindowCallback: function(ractiveControl){
-      $("#properties-container .headers").change(function(ev){
-            var items = $("#properties-container .headers").val().split(",");
+      $("#properties-container .headers").change(function(){
+            var itemsString = $("#properties-container .headers").val();
+            var items = itemsString.split(",");
             var formattedItems = [];
             for(var i=0;i<items.length;i++){
                 var item = items[i];
