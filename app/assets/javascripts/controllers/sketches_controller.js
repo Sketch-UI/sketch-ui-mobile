b2b77c3054358929bcfd1d4e85c12fc5969f3e69
Inderpal Singh <inderps@thoughtworks.com>
2014-01-19 20:49:19 +0530

    now saving the drawing board size swell

--- app/assets/javascripts/controllers/sketches_controller.js
+++ app/assets/javascripts/controllers/sketches_controller.js
@@ -1,16 +1,18 @@
 var SketchesController = (function() {
 
     var show = function(){
-        var drawingBoardPositions = $("#drawing-board")[0].getBoundingClientRect();
         sketchId = $("#identifier").val();
         Sketch.find(sketchId, function(data){
             if(data.controls_data){
-                for(var i=0; i<data.controls_data.length; i++){
-                    var sidebarLeft = 0;
-                    if($("#toolbox-left").is(":visible")){
-                        sidebarLeft = 260;
-                    }
+                DrawingBoard.controls[1].set(data.controls_data[0].properties);
+                var drawingBoardPositions = $("#drawing-board")[0].getBoundingClientRect();
 
+                for(var i=1; i<data.controls_data.length; i++){
+//                    var sidebarLeft = 0;
+//                    if($("#toolbox-left").is(":visible")){
+//                        sidebarLeft = 260;
+//                    }
+//
                     var postion = {
                         top: drawingBoardPositions.top + parseInt(data.controls_data[i].position.top),
                         left: drawingBoardPositions.left + parseInt(data.controls_data[i].position.left)
@@ -31,7 +33,11 @@ var SketchesController = (function() {
 
         for(var key in controls){
             if(key=="1"){
-
+                var controlData = {
+                    id: key,
+                    properties: controls[key].get()
+                }
+                sketchData.push(controlData);
             }
             else{
                 var $control = $("#drawing-board .control[data-control-id='"+key+"']");
@@ -52,7 +58,7 @@ var SketchesController = (function() {
                     properties: controls[key].get(),
                     position: position
                 }
-                sketchData.push(controlData)
+                sketchData.push(controlData);
             }
         }
 
