fdc1827e02548c6a4e0019dd1b58cd4b03325e7b
Pallavi Malik <pallavimalik2@gmail.com>
2014-01-24 19:51:23 +0530

    |PM IPS|Adding Header

---
+++ app/assets/javascripts/controls/headerControl.js
@@ -0,0 +1,9 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['HeaderControl'] = {
+  initialData: {
+      bgcolor: "#4a87ee",
+      title: "Header",
+      color: "#ffffff"
+  },
+  propertyWindowCallback: function(ractiveControl){}
+};
