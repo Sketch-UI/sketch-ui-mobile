dda6ee4a153e083a472dee352e73f7aae044384d
Pallavi Malik <pallavimalik2@gmail.com>
2014-01-24 19:22:11 +0530

    added ionic framework (css only)

--- app/assets/javascripts/controls/drawingBoardControl.js
+++ app/assets/javascripts/controls/drawingBoardControl.js
@@ -1,8 +1,8 @@
 window.ControlsMetadata = window.ControlsMetadata || {};
 window.ControlsMetadata['DrawingBoardControl'] = {
   initialData: {
-      width: "1200",
-      height: "800"
+      width: "328",
+      height: "576"
   },
   propertyWindowCallback: function(ractiveControl){
   }
