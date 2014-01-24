98ad986c0f3ccc5f211f6dcfd3da07ac11c81293
Vaibhav Khunger <Vaibhav@invaibhavk.corporate.thoughtworks.com>
2014-01-24 20:08:17 +0530

    Adding Button Control

---
+++ app/assets/javascripts/controls/buttonControl.js
@@ -0,0 +1,10 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['ButtonControl'] = {
+  initialData: {
+      button_label: "Button",
+      bgcolor: "#428bca",
+      color: "#ffffff",
+      width: 90
+  },
+  propertyWindowCallback: function(ractiveControl){}
+};
