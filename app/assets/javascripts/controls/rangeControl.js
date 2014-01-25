df9ec5dc3769e7d6fbd7122748bd49bc67c23257
Pallavi Malik <pallavimalik2@gmail.com>
2014-01-25 11:39:24 +0530

    |PS PM|Adding range control

---
+++ app/assets/javascripts/controls/rangeControl.js
@@ -0,0 +1,8 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['RangeControl'] = {
+  initialData: {
+      width: 200,
+      color: "#000000"
+  },
+  propertyWindowCallback: function(ractiveControl){}
+};
