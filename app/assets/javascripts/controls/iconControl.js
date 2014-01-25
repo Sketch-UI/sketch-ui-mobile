b3642f61190892201bfc7fbbc4cff26b6944b6cb
Pallavi Malik <pallavimalik2@gmail.com>
2014-01-25 19:44:13 +0530

    Adding all icon list

--- app/assets/javascripts/controls/iconControl.js
+++ app/assets/javascripts/controls/iconControl.js
@@ -2,7 +2,8 @@ window.ControlsMetadata = window.ControlsMetadata || {};
 window.ControlsMetadata['IconControl'] = {
   initialData: {
       font_size: "30",
-      color: "#000000"
+      color: "#000000",
+      icon_class: "ion-star"
   },
   propertyWindowCallback: function(ractiveControl){}
 };
