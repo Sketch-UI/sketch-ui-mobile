759dd0c7cdef1564012009e8dc5975b0f90472aa
Inderpal Singh <inderps@thoughtworks.com>
2014-01-25 15:30:24 +0530

    added checkbox control

---
+++ app/assets/javascripts/controls/checkboxControl.js
@@ -0,0 +1,8 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['CheckboxControl'] = {
+  initialData: {
+      selected: true,
+      color: "#000000"
+  },
+  propertyWindowCallback: function(ractiveControl){}
+};
