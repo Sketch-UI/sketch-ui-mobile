ccaeef091737c271bc6f65c17cde3cd01de3eea9
kushjain@thoughtworks.com <Mr.KushJain@gmail.com>
2014-01-25 16:41:29 +0530

    |#4|KJ|Added toggle control

---
+++ app/assets/javascripts/controls/toggleControl.js
@@ -0,0 +1,29 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['ToggleControl'] = {
+    initialData: {
+        width: 320,
+        items: [
+            { text: "Item1"},
+            { text: "Item2"},
+            { text: "Item3"},
+            { text: "Item4"}
+        ]
+
+    },
+    propertyWindowCallback: function (ractiveControl) {
+        $("#dropdown-items").val("Item1\nItem2\nItem3\nItem4");
+        $("#dropdown-items").change(function (ev) {
+            var items = $("#dropdown-items").val().split("\n");
+            var formattedItems = [];
+            for (var i = 0; i < items.length; i++) {
+
+
+                formattedItems.push({
+                    text: items[i]
+                });
+            }
+            ractiveControl.set("items", formattedItems);
+        });
+
+    }
+};
