f630febe03f025b959f07d60bb849b2fec393d82
Inderpal Singh <inderps@thoughtworks.com>
2014-01-25 14:47:30 +0530

    removing checkbox control and images

--- app/assets/javascripts/controls/checkBoxControl.js
+++
@@ -1,9 +0,0 @@
-window.ControlsMetadata = window.ControlsMetadata || {};
-window.ControlsMetadata['CheckBoxControl'] = {
-  initialData: {
-      selected: false,
-      caption: "Checkbox",
-      color: "#000000"
-  },
-  propertyWindowCallback: function(ractiveControl){}
-};
