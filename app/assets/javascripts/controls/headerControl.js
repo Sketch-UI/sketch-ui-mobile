d60cec92cdf6d7d30dae08429837ef4d145dabee
Inderpal Singh <inderps@thoughtworks.com>
2014-01-29 18:28:33 +0530

    removed header control

--- app/assets/javascripts/controls/headerControl.js
+++
@@ -1,9 +0,0 @@
-window.ControlsMetadata = window.ControlsMetadata || {};
-window.ControlsMetadata['HeaderControl'] = {
-  initialData: {
-      bgcolor: "#4a87ee",
-      title: "Header",
-      color: "#ffffff"
-  },
-  propertyWindowCallback: function(ractiveControl){}
-};
