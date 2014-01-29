924c095d20d65f9630a2e3db9c5f96cdee013e16
Pallavi <pallavimalik@pallavi.local>
2014-01-29 21:26:11 +0530

    Add button group control

---
+++ app/assets/javascripts/controls/buttongroupControl.js
@@ -0,0 +1,31 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['ButtongroupControl'] = {
+    initialData: {
+        width: 80,
+        items: [
+            { text: "Item1"},
+            { text: "Item2"},
+            { text: "Item3"},
+            { text: "Item4"}
+        ],
+        bg_color: '#4a87ee',
+        color: '#e5e5e5'
+
+    },
+
+    propertyWindowCallback: function (ractiveControl) {
+        $("#dropdown-items").val("Item1\nItem2\nItem3\nItem4");
+        $("#dropdown-items").change(function (ev) {
+            var items = $("#dropdown-items").val().split("\n");
+            var formattedItems = [];
+            for (var i = 0; i < items.length; i++) {
+
+
+                formattedItems.push({
+                    text: items[i]
+                });
+            }
+            ractiveControl.set("items", formattedItems);
+        });
+    }
+};
