8b2437c2b920869bd5994f2182bf26f0e7b86b0f
pallavimalik <pallavimalik2@gmail.com>
2014-02-10 20:39:45 +0530

    Adding paragraph control

---
+++ app/assets/javascripts/controls/paragraphControls.js
@@ -0,0 +1,10 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['ParagraphControls'] = {
+  initialData: {
+      text: "paragraph control",
+      width: "320",
+      height: "100",
+      align: "left"
+  },
+  propertyWindowCallback: function(ractiveControl){}
+};
