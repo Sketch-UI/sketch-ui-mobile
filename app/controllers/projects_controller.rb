f53e7431a38827445c3d1e192ee3e38eab913819
Inderpal Singh <inderps@thoughtworks.com>
2014-02-10 21:04:17 +0530

    removed height width for now

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 class ProjectsController < ApplicationController
   def create
-    project = Project.create(name: params[:project_name], width: params[:width], height: params[:height], project_id: SecureRandom.hex)
+    project = Project.create(name: params[:project_name], project_id: SecureRandom.hex)
     project.sketches.create(sketch_id: "1", name: "Homepage")
     redirect_to "/projects/#{project.project_id}#1"
   end
