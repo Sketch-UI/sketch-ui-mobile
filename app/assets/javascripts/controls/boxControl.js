1ae1d068329c95738f99148731890a23b23baef7
pallavimalik <pallavimalik2@gmail.com>
2014-02-10 19:36:30 +0530

    Added box control

---
+++ app/assets/javascripts/controls/boxControl.js
@@ -0,0 +1,12 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['BoxControl'] = {
+  initialData: {
+      size: 1,
+      color: "#000000",
+      width: "70",
+      height: "60",
+      bg_color: "#4a87ee",
+      border_radius: 0
+  },
+  propertyWindowCallback: function(ractiveControl){}
+};
