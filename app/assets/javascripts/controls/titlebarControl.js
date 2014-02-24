dac29a3d8262d1b08c231c8c77ac0e546fc8cdc0
Inderpal Singh <inderps@thoughtworks.com>
2014-02-24 23:03:31 +0530

    added titlebar control

---
+++ app/assets/javascripts/controls/titlebarControl.js
@@ -0,0 +1,54 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['TitlebarControl'] = {
+  groupControl: true,
+  controls: [
+      {
+          metadataId: "ToolbarControl",
+          position: {
+              left: 0,
+              top: 0
+          },
+          data: {
+              bgcolor: "#4a87ee",
+              height: "44"
+          }
+      },
+       {
+            metadataId: "IconControl",
+            position: {
+                left: 6,
+                top: 2
+            },
+            data: {
+                font_size: "30",
+                color: "#ffffff",
+                icon_class: "ion-arrow-left-a"
+            }
+      },
+      {
+          metadataId: "IconControl",
+          position: {
+              left: 290,
+              top: 0
+          },
+          data: {
+              font_size: "30",
+              color: "#ffffff",
+              icon_class: "ion-navicon-round"
+          }
+      },
+      {
+          metadataId: "LabelControl",
+          position: {
+              left: -170,
+              top: 0
+          },
+          data: {
+              color: "#ffffff",
+              size: "22",
+              label: "Titlebar",
+              font: "Verdana"
+          }
+      }
+  ]
+};
