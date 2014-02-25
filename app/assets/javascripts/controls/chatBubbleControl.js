4109283d0d15a1315702a21591288d031ef52d40
Kush Jain <kushjain@thoughtworks.com>
2014-02-25 23:13:57 +0530

    |#0|KJ|Added Bubble Chat Control

---
+++ app/assets/javascripts/controls/chatBubbleControl.js
@@ -0,0 +1,12 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['ChatBubbleControl'] = {
+  initialData: {
+      bg_color: "rgb(238, 184, 61)",
+      text_color: "#000000",
+      caption: "Hey Buddy! Have a great day",
+      right_bubble: false,
+      top_pointer:true
+
+  },
+  propertyWindowCallback: function(ractiveControl){}
+};
