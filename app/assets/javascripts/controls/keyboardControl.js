2f54a63861c7140d1c7e36a3a674074ffec9ff13
Ruchi Mutneja <ruchimut@thoughtworks.com>
2014-03-03 21:50:43 +0530

    Adding keyboard control

---
+++ app/assets/javascripts/controls/keyboardControl.js
@@ -0,0 +1,7 @@
+window.ControlsMetadata = window.ControlsMetadata || {};
+window.ControlsMetadata['KeyboardControl'] = {
+  initialData: {
+      phone: 'android.png'
+  },
+  propertyWindowCallback: function(ractiveControl){}
+};
